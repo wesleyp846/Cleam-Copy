259c97cf6d14ae9ab9d7c611cafe1e21082e4b36
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-05-02 18:27:56 -0300

    atualização geral

--- duplic.py
+++ duplic.py
@@ -1,52 +1,33 @@
 import os
-import hashlib
 import shutil
 
-folder_path = "C:/"
+folder_path = "E:/"
 duplicates_folder = os.path.join(folder_path, "duplicados")
 
 file_sizes = {}
-file_hashes = {}
-
-duplicates = []
 
 for file_name in os.listdir(folder_path):
     file_path = os.path.join(folder_path, file_name)
     if os.path.isfile(file_path):
         file_size = os.path.getsize(file_path)
-        file_sizes[file_name] = file_size
-        with open(file_path, 'rb') as f:
-            file_hash = hashlib.md5(f.read()).hexdigest()
-            file_hashes[file_name] = file_hash
-
-        # Detecta arquivos duplicados
-        if file_size in file_sizes.values():
-            for other_file_name, other_file_size in file_sizes.items():
-                if file_size == other_file_size and file_name != other_file_name:
-                    if file_hashes[file_name] == file_hashes[other_file_name]:
-                        if sorted([file_name, other_file_name]) not in duplicates:
-                            duplicates.append(sorted([file_name, other_file_name]))
-
-# Move arquivos duplicados para a pasta "duplicados"
-if duplicates:
-    print("Arquivos duplicados encontrados. Movendo para a pasta 'duplicados'...")
-    if not os.path.exists(duplicates_folder):
-        os.mkdir(duplicates_folder)
-    for pair in duplicates:
-        for file_name in pair:
-            old_path = os.path.join(folder_path, file_name)
-            new_path = os.path.join(duplicates_folder, file_name)
-            shutil.move(old_path, new_path)
-    print("Arquivos duplicados movidos com sucesso para a pasta 'duplicados'.")
-else:
-    print("Nenhum arquivo duplicado encontrado.")
+        # Define a faixa de tamanhos de arquivo a ser considerada como duplicata
+        size_range = range(file_size - 2048, file_size + 2049)
+        found_duplicate = False
+        for size in file_sizes.keys():
+            if size in size_range:
+                found_duplicate = True
+                # Arquivo duplicado encontrado
+                duplicate_name = f"{size}_{len(file_sizes[size])}"
+                duplicate_path = os.path.join(duplicates_folder, duplicate_name)
+                shutil.move(file_path, duplicate_path)
+                file_sizes[size].append(duplicate_name)
+                break
+        if not found_duplicate:
+            file_sizes[file_size] = [file_name]
 
 # Imprime tamanhos de arquivos
 print("\nTamanhos de arquivos:")
-for file_name, file_size in file_sizes.items():
-    print(f"{file_name}: {file_size} bytes")
-
-# Imprime hashes de arquivos
-print("\nHashes de arquivos:")
-for file_name, file_hash in file_hashes.items():
-    print(f"{file_name}: {file_hash}")
+for file_size, file_names in file_sizes.items():
+    print(f"{file_size} bytes:")
+    for file_name in file_names:
+        print(f" - {file_name}")
